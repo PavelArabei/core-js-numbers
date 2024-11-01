01ce8dee93f2cb90ed55dee07aa98b114d49fcf7
Pashtet <ptashkaaaaaa@gmail.com>
2024-11-01 21:02:19 +0100

    feat: install eslint-plugin-jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     node: true,
     mocha: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,6 +18,9 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
+    'jsdoc/require-param-description': 0,
+    'jsdoc/require-returns-description': 0,
+    'jsdoc/check-tag-names': 0,
     'prettier/prettier': [
       'error',
       {
@@ -21,6 +28,6 @@ module.exports = {
       },
     ],
   },
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jsdoc'],
   noInlineConfig: true,
 };
